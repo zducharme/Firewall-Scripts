393a8276c4c959a764ef4ef84b7653aedb6de04a
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:27:12 -0500

    Update FirewallScript_4.sh

--- FirewallScript_4.sh
+++ FirewallScript_4.sh
@@ -1,1 +1,8 @@
+#
+## Firewall rule for allowing/denying incoming SMTP/POP/IMAP connections.
+##
 
+#!/bin/bash
+sudo iptables -A INPUT -p tcp --match multiport --dport 25,587 -j ACCEPT
+sudo iptables -A INPUT -p tcp --match multiport --dport 110,995 -j ACCEPT
+sudo iptables -A INPUT -p tcp --match multiport --dport 143,993 -j ACCEPT
