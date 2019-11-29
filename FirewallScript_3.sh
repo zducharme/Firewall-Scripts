b49b014e802dbea95af459882d45f552faa6161d
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:26:45 -0500

    Update FirewallScript_3.sh

--- FirewallScript_3.sh
+++ FirewallScript_3.sh
@@ -1,1 +1,7 @@
+##
+## Firewall rule for allowing incoming and outgoing SSH connections.
+##
 
+#!/bin/bash
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 22 -j REJECT
