0ae00bbbaf4d7ded6205229403515678741f4b23
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:27:40 -0500

    Update FirewallScript_5.sh

--- FirewallScript_5.sh
+++ FirewallScript_5.sh
@@ -1,1 +1,13 @@
+#
+## Allow/Block Specific Hosts (MAC, Telnet, ICMP... EX. Port = 1996 )
+##
 
+#!/bin/bash
+sudo iptables -A INPUT -p tcp --dport 1996 -m mac --mac-source 00:0F:EA:91:04:07 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 1996 -m mac --mac-source 00:0F:EA:91:04:07 -j REJECT
+
+sudo iptables -A INPUT -p tcp --dport 23 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 23 -j REJECT
+
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+sudo iptables -A OUTPUT -p icmp --icmp-type echo-request -j REJECT
