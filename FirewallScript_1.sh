9fa0652b2b668418269823620a7aebef6c925221
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 18:34:39 -0500

    Update FirewallScript_1.sh

--- FirewallScript_1.sh
+++ FirewallScript_1.sh
@@ -1,6 +1,5 @@
-## Zachary Ducharme 11/6/19
 ## Firewall rule for opening web server ports; forwarded to port 8080.
-##
+## Zachary Ducharme 11/6/19
 
 #!/bin/bash
 sudo iptables -t nat -A INPUT -p tcp --dport 80 -j REDIRECT --to-port 8080
